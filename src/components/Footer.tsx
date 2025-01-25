2a363f12b1eb6c1c34a76b677f39c7fcd43285d6
prodjohnper <JonathanPerez9743@gmail.com>
2025-01-24 22:51:42 -0400

    Add - Created `Header.tsx` and `Footer.tsx`. Updated `layout.tsx` file

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="p-5">
+      <p>Footer</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="p-5">
+      <h1>Header</h1>
+    </header>
+  )
+}
+
+export default Header
