a34292c5a47bf09e3f82ae931de4d12bb8c7ea04
prodjohnper <JonathanPerez9743@gmail.com>
2025-01-27 14:12:50 -0400

    Add - Created `forgot-password` route

---
+++ src/app/forgot-password/page.tsx
@@ -0,0 +1,34 @@
+import './styles/forgot-password.css';
+
+const ForgotPassword = () => {
+  return (
+    <main className="p-5 h-full flex flex-col justify-center items-center">
+      <div className="flex flex-col items-center justify-center card">
+        <h1 className='text-4xl'>Forgot Password</h1>
+        <p className='text-wrap -mt-2 mb-8 max-w-72 text-center text-black/50'>Enter the email address associated with your account.</p>
+        <form action="#" className='flex flex-col gap-2'>
+          <div className='text-left'>
+            {/* Email */}
+            <label htmlFor="email"> Email
+              <input type="email" id='email'/>
+            </label>
+            <div className='-mt-4 mb-6 flex justify-end'>
+              <a href="/support" className='text-xs hover:underline'>
+                Need help?
+              </a>
+            </div>
+          </div>
+
+          <div className='flex flex-col gap-2'>
+            {/* Button */}
+            <a href="">
+              <button className='w-full'>Send</button>
+            </a>
+          </div>
+        </form>
+      </div>
+    </main>
+  )
+}
+
+export default ForgotPassword
