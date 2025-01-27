37b52afeb6533684e4f65977e3beaa29aacb4d3d
prodjohnper <JonathanPerez9743@gmail.com>
2025-01-27 14:14:35 -0400

    Update - `link` implementation in header for easier acces to home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,13 @@
+import Link from 'next/link'
+
 const Header = () => {
   return (
     <header className="p-5">
-      <h1>Header</h1>
+      <Link href="/">
+        <p>Header</p>
+      </Link>
     </header>
-  )
+  );
 }
 
 export default Header
