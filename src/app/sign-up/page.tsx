96e7595a7c28d54c643ce75b0443db87769ca19a
Lusanco <93743011+Lusanco@users.noreply.github.com>
2025-01-28 18:33:52 -0400

    update - css module import

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,46 +1,60 @@
-import './styles/sign-up.css';
+import "./styles/sign-up.module.css";
 
 const SignUp = () => {
   return (
     <main className="p-5 flex flex-col justify-center items-center h-full">
       <section className="flex flex-col items-center justify-center card">
-        <div className='flex flex-col text-center'>
-          <h1 className='text-5xl'>Sign Up</h1>
-          <p className='-mt-2 text-lg text-black/50'>Create an account</p>
+        <div className="flex flex-col text-center">
+          <h1 className="text-5xl">Sign Up</h1>
+          <p className="-mt-2 text-lg text-black/50">Create an account</p>
         </div>
-        <form action="#" className='flex flex-col gap-2'>
+        <form action="#" className="flex flex-col gap-2">
           <div>
             {/* Name */}
-            <label htmlFor="name"> Name
-              <input type="text" id='name' placeholder='John'/>
+            <label htmlFor="name">
+              {" "}
+              Name
+              <input type="text" id="name" placeholder="John" />
             </label>
 
             {/* Last Name */}
-            <label htmlFor="last-name"> Last Name
-              <input type="text" id='last-name' placeholder='Doe'/>
+            <label htmlFor="last-name">
+              {" "}
+              Last Name
+              <input type="text" id="last-name" placeholder="Doe" />
             </label>
 
             {/* Email */}
-            <label htmlFor="email"> Email
-              <input type="email" id='email' placeholder='example@mail.com'/>
+            <label htmlFor="email">
+              {" "}
+              Email
+              <input type="email" id="email" placeholder="example@mail.com" />
             </label>
 
             {/* Password */}
-            <label htmlFor="password"> Password
-              <input type="password" id='password' placeholder='********'/>
+            <label htmlFor="password">
+              {" "}
+              Password
+              <input type="password" id="password" placeholder="********" />
             </label>
 
             {/* Confirm Password */}
-            <label htmlFor="confirm-password"> Confirm Password
-              <input type="password" id='confirm-password' placeholder='********'/>
+            <label htmlFor="confirm-password">
+              {" "}
+              Confirm Password
+              <input
+                type="password"
+                id="confirm-password"
+                placeholder="********"
+              />
             </label>
           </div>
-          
-          <div className='flex flex-col gap-2'>
+
+          <div className="flex flex-col gap-2">
             <a href="">
-              <button className='w-full'>Sign Up</button>
+              <button className="w-full">Sign Up</button>
             </a>
-            <a href="/login" className='text-xs hover:underline text-center'>
+            <a href="/login" className="text-xs hover:underline text-center">
               Already have an account?
             </a>
           </div>
@@ -48,6 +62,6 @@ const SignUp = () => {
       </section>
     </main>
   );
-}
+};
 
-export default SignUp
+export default SignUp;
