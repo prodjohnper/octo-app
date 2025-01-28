60c0292841812802b5a5ac2d75bbe5ecffec9aea
Lusanco <93743011+Lusanco@users.noreply.github.com>
2025-01-28 13:11:16 -0400

    add - IProduct interface

---
+++ src/app/products/interfaces/IProduct.ts
@@ -0,0 +1,13 @@
+interface IProduct {
+  id: number;
+  name: string;
+  description: string;
+  image: string;
+  quantity: number;
+  category: string;
+  category_id: number;
+  created_at: string;
+  updated_at: string;
+}
+
+export default IProduct;
