87dc9dceab5f23f7be2dc19bfa9c683030c8b3e7
prodjohnper <JonathanPerez9743@gmail.com>
2025-01-27 21:42:45 -0400

    Fix - Fixed typos in route `orders`

--- src/app/orders/[orderDetails]/page.tsx
+++ src/app/orders/[orderDetails]/page.tsx
@@ -1,6 +1,6 @@
 const OrderDetails = ({ params }: { params: { orderDetails: string } }) => {
   return (
-    <main>
+    <main className="p-5">
       <h1>Order {params.orderDetails} details</h1>
     </main>
   );
--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -10,11 +10,11 @@ const ordersList = [
 const Orders = () => {
   return (
     <main className="p-5">
-      <h1>Products page</h1>
+      <h1>Orders page</h1>
       <div className="flex flex-col ">
         <ul className="grid grid-cols-1 md:grid-cols-3 gap-4"> 
           {ordersList.map(order => (
-            <a href={`/products/${order.id}`} key={order.id}>
+            <a href={`/orders/${order.id}`} key={order.id}>
               <li className="list-none p-5 bg-white rounded-md shadow-md hover:shadow-lg transition-shadow">{order.name}</li>
             </a>
           ))}
