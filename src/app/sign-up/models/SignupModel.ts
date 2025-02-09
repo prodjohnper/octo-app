dfd0d8b6410de17e48ffea8d0934a68fe67b20c4
prodjohnper <JonathanPerez9743@gmail.com>
2025-02-08 20:17:51 -0400

    Delete - Removed sign-up route, refactored entirely in signup

--- src/app/sign-up/models/SignupModel.ts
+++
@@ -1,9 +0,0 @@
-import { ISignupRequest, ISignupResponse } from "../interfaces/ISignup";
-
-export class SignupRequestModel implements ISignupRequest {
-  constructor(public data: { email: string; password: string }) {}
-}
-
-export class SignupResponseModel implements ISignupResponse {
-  constructor(public data: { detail: string; status: string }) {}
-}
